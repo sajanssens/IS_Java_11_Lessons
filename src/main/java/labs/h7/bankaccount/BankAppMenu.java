08a0b634a238a17205a46a909003145092b0b54f
AT-PC\Asare_Dev <asare.y.e.twum@gmail.com>
2021-03-18 11:22:51 +0100

    Added BankAccountMenu

---
+++ src/main/java/labs/h7/bankaccount/BankAppMenu.java
@@ -0,0 +1,15 @@
+package labs.h7.bankaccount;
+
+public class BankAppMenu {
+
+    public void mainMenu(){
+
+        System.out.println("Welcome what would you like to do?" +
+                "\n1)" +
+                "\n2)" +
+                "\n3)" +
+                "\n4)" +
+                "\n0)");
+
+    }
+}
